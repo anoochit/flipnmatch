51127c48507082f2420e7791f48f6ec35458113f
Anuchit Chalothorn <anoochit@gmail.com>
2024-05-28 14:24:19 +0700

    add desktop support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
-import 'package:flipnmatch/pages/home.dart';
+import 'dart:io';
+
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flipnmatch/pages/start.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
+  // Check if the platform is Windows, Linux, or MacOS
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // Wait until the window is ready
+    doWhenWindowReady(() {
+      // Set the initial size of the window
+      const initialSize = Size(405, 720);
+      // Set the maximum size of the window
+      appWindow.maxSize = initialSize;
+      // Set the minimum size of the window
+      appWindow.minSize = initialSize;
+      // Set the initial size of the window
+      appWindow.size = initialSize;
+      // Show the window
+      appWindow.show();
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
